dca198928effc37028d104e9c1a3477485a1b83f
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-07 16:50:30 +0200

    wrote a readme file

---
+++ hello.js
@@ -0,0 +1,10 @@
+/*
+* @Author: TBtuo
+* @Date:   2016-07-07 16:48:52
+* @Last Modified by:   TBtuo
+* @Last Modified time: 2016-07-07 16:49:21
+*/
+
+'use strict';
+
+Git is a version control system
